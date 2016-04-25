9e858ee916456c1eb4ba71a52108979b6d3b8ee2
David Yao <njlxyaoxinwei@gmail.com>
2016-04-25 14:32:38 -0400

    add files for simple viewer

---
+++ test_simpleViewer.cpp
@@ -0,0 +1,18 @@
+#include "simpleViewer.h"
+#include <qapplication.h>
+
+int main(int argc, char** argv) {
+  // Read command lines arguments.
+  QApplication app(argc, argv);
+  
+  // Instantiate the Viewer.
+  Viewer viewer;
+  
+  viewer.setWindowTitle("Mesh Viewer");
+  
+  // Make the viewer window visible on screen.
+  viewer.show();
+
+  // Run main loop.
+  return app.exec();
+}
