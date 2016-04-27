df5d049a1be459af266bc9399ac671f68f3097e8
David Yao <njlxyaoxinwei@gmail.com>
2016-04-27 11:36:28 -0400

    real time directional light

--- shadowViewer.cpp
+++ shadowViewer.cpp
@@ -13,6 +13,19 @@ namespace {
   using qglviewer::Vec;
   using glm::make_mat4;
   using glm::vec4;
+  using glm::vec3;
+  using glm::vec2;
+  using glm::mat4;
+
+  vec3 ApplyMat4(const vec3& p, const mat4& mat) {
+    vec3 np(mat * vec4(p, 1));
+    return np;
+  }
+
+  vec2 ProjectXY(const vec3& p, const vec3& pos) {
+    float t = - p.z / pos.z;
+    return vec2(p.x + t * pos.x, p.y + t * pos.y);
+  }
 } // namespace
 
 ShadowViewer::ShadowViewer(const Scene* const s,
@@ -27,7 +40,8 @@ ShadowViewer::ShadowViewer(const Scene* const s,
 void ShadowViewer::draw_shadow_(const Mesh& mesh, const Vec& pos) {
   const auto nTriangles = mesh.indices.size() / 3;
   glBegin(GL_TRIANGLES);
-  glm::mat4 mat = make_mat4(scene_->mesh_frame_->worldMatrix());
+  mat4 mat = make_mat4(scene_->mesh_frame_->worldMatrix());
+  vec3 light_dir = vec3(-pos.x, -pos.y, -pos.z);
   for (unsigned int i = 0; i < nTriangles; i++) {
     unsigned int j = 3 * i;
     unsigned int is[3] = {
@@ -36,20 +50,25 @@ void ShadowViewer::draw_shadow_(const Mesh& mesh, const Vec& pos) {
       mesh.indices[j+2]
     };
     glColor3f(0.0f, 0.0f, 0.0f);
-    glm::vec3 v[3] = {
-      mesh.vertices[is[0]].Position,
-      mesh.vertices[is[1]].Position,
-      mesh.vertices[is[2]].Position
+    Vertex v[3] = {
+      mesh.vertices[is[0]],
+      mesh.vertices[is[1]],
+      mesh.vertices[is[2]]
     };
-    glm::vec4 w[3] = {
-      (mat * vec4(v[0], 1)),
-      (mat * vec4(v[1], 1)),
-      (mat * vec4(v[2], 1))
+    vec3 w[3] = {
+      ApplyMat4(v[0].Position, mat),
+      ApplyMat4(v[1].Position, mat),
+      ApplyMat4(v[2].Position, mat)
     };
     // glNormal3f(v[0].Normal.x, v[0].Normal.y, v[0].Normal.z);
-    glVertex2f(w[0].x, w[0].y);
-    glVertex2f(w[1].x, w[1].y);
-    glVertex2f(w[2].x, w[2].y);
+    vec2 s[3] = {
+      ProjectXY(w[0], light_dir),
+      ProjectXY(w[1], light_dir),
+      ProjectXY(w[2], light_dir)
+    };
+    glVertex2f(s[0].x, s[0].y);
+    glVertex2f(s[1].x, s[1].y);
+    glVertex2f(s[2].x, s[2].y);
   }
   glEnd();
 }
